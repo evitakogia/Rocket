15c41ea25102d59bdcfa10434538f9d20da5b40c
Marios Kogias <marioskogias@gmail.com>
2021-08-19 17:32:02 +0100

    Send data to remote server

--- groundstation/datafromground.py
+++ groundstation/datafromground.py
@@ -1,34 +1,42 @@
 import serial
 import json
+import requests
 
-def json_data(data):
-    l = list(data)
-    x = {
-        "Temperature": l[0],
-        "Pressure": l[1],
-        "Altitude": l[2]
+SERIAL = 'COM7'
+RATE = 9600
+SERVER = "127.0.0.1:8000"
+
+
+def create_json_body(data):
+    list_data = list(data)
+    body = {
+        "Temperature": list_data[0],
+        "Pressure": list_data[1],
+        "Altitude": list_data[2]
     }
-    print(json.dumps(x))
-   
+    return json.dumps(body)
+
+
 def read_from_port(ser):
     reading = ""
     while True:
         reading += ser.readline().decode()
         if reading != "":
-            print(reading)
             data = reading.split(" ")
             if len(data) == 3:
-                json_data(map(float, data))
-                reading = ""
+                return map(float, data)
+
 
 def main():
     print("Hello Rocket")
-    port = 'COM7'
-    baud = 9600
+    serial_port = serial.Serial(SERIAL, RATE, timeout=0)
 
-    serial_port = serial.Serial(port, baud, timeout=0)
+    # Read data from serial and send them in JSON to server
+    while True:
+        data = read_from_port(serial_port)
+        body = create_json_body(data)
+        requests.post(SERVER, json=body)
 
-    read_from_port(serial_port)
-    
+
 if __name__ == "__main__":
     main()
